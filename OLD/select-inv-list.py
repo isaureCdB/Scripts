b3b701d709d7d8cfd7f46a36ac1b9deb4bad7129
CHAUVOT DE BEAUCHENE Isaure <isaure.chauvot-de-beauchene@loria.fr>
2019-06-23 11:07:52 +0200

    clean-up

--- OLD/select-inv-list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import sys
-
-chains = open(sys.argv[1]).readlines()
-clashing = set([int(l.split()[0])-1 for l in open(sys.argv[2]).readlines()])
-
-for nc, c in enumerate(chains):
-    if nc not in clashing:
-        print(c, end=' ')
