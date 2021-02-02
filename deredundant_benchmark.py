8a318d9f243c6f64384653804f1db7fb3e441073
CHAUVOT DE BEAUCHENE Isaure <isaure.chauvot-de-beauchene@loria.fr>
2021-02-02 16:09:10 +0100

    fix bug

--- deredundant_benchmark.py
+++ deredundant_benchmark.py
@@ -11,35 +11,51 @@ seq = [l[3] for l in ll]
 '''
 p_struc = "xxxx"
 p_prot = "xxxx"
+p_seq = "xxx"
 tokeep = []
 
+def dr(tokeep):
+	for l1 in tokeep:
+		seq1 = l1.split()[3]
+		for l2 in tokeep:
+			if l1 == l2:
+				continue
+			seq2 = l2.split()[3]
+			if seq1 in seq2:
+				#print(l1,l2)
+				tokeep.remove(l1)
+				break
+			elif seq2 in seq1:
+				#print(l2,l1)
+				tokeep.remove(l2)
+	return tokeep
+
+
 for ll in lines:
-    l = ll.split()
-    if len(l) != 4:
-        continue
-    struc, prot, rna, seq = l
-    #Keep only first prot chain per structure
-    if struc == p_struc and p_prot != prot:
-        continue
-    #Compare RNA sequences bound to same or similar prot
-    #If one seq contains the other, discard shortest
-    if struc[:3] != p_struc[:3]:
-        for l1 in tokeep:
-            seq1 = l1.split()[3]
-            for l2 in tokeep:
-                if l1 == l2:
-                    continue
-                seq2 = l2.split()[3]
-                if seq1 in seq2:
-                    #print(l1,l2)
-                    tokeep.remove(l1)
-                    break
-                elif seq2 in seq1:
-                    #print(l2,l1)
-                    tokeep.remove(l2)
-        for l in tokeep:
-            print(l,end="")
-        tokeep = []
-    tokeep.append(ll)
-    p_struc = struc
-    p_prot = prot
+	l = ll.split()
+	if len(l) != 4:
+		continue
+	struc, prot, rna, seq = l
+	#Keep only first prot chain per structure
+	if struc == p_struc and p_prot != prot:
+		continue
+	#remove similar pdb with same RNA seq
+	if struc[:3] == p_struc[:3] and p_seq == seq:
+		continue
+	#Compare RNA sequences bound to same or similar prot
+	#If one seq contains the other, discard shortest
+	if struc[:3] != p_struc[:3]:
+		tokeep = dr(tokeep)
+		tokeep = dr(tokeep)
+		for l in tokeep:
+			print(l,end="")
+		tokeep = []
+	tokeep.append(ll)
+	p_struc = struc
+	p_prot = prot
+	p_seq = seq
+	
+tokeep = dr(tokeep)
+tokeep = dr(tokeep)
+for l in tokeep:
+	print(l,end="")
